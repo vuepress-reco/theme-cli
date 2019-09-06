78419dad605e620b414b7c9b97a0aa36a32432e7
reco_luan <recoluan@qq.com>
2019-09-06 17:29:23 +0800

    add: Customize style/title/description/author

--- bin/inquirer.js
+++ bin/inquirer.js
@@ -2,10 +2,25 @@ const inquirer = require('inquirer');
 
 const questions = [
   {
+    name: 'title',
+    type: 'input',
+    message: `What's the title of your project?`
+  },
+  {
+    name: 'description',
+    type: 'input',
+    message: `What's the description of your project?`
+  },
+  {
+    name: 'author',
+    type: 'input',
+    message: `What's the author's name?`
+  },
+  {
+    name: 'style',
     type: 'list',
-    name: 'version',
-    message: 'Which version you need?',
-    choices: ['1.x', '0.x'],
+    message: `What style do you want your home page to be?(Select afternoon-grocery, if you want to download reco_luan's '午后南杂')`,
+    choices: ['blog', 'doc', 'afternoon-grocery'],
     filter: function(val) {
       return val.toLowerCase();
     }
@@ -17,7 +32,7 @@ const handleInquirer = function () {
     inquirer
       .prompt(questions)
       .then(answers => {
-        resolve(answers.version)
+        resolve(answers)
       })
       .catch(err => {
         reject(err)
--- bin/reco-build.js
+++ bin/reco-build.js
@@ -7,9 +7,13 @@ const ora = require('ora');
 const fs = require('fs');
 
 const handleInquirer = require('./inquirer.js')
+const spinner = ora()
+
+let stepNum = 3
+let currentStep = 1
 
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .option('-i, init [name]', '初始化 vuepress-theme-reco 主题博客')
   .parse(process.argv);
 
@@ -23,25 +27,20 @@ program.on('--help', () => {
 
 if (program.init) {
   handleInquirer()
-    .then(res => {
-      const gitBranch = `recoluan/vuepress-theme-reco-demo#demo/${res}`
-      const spinner = ora().start();
-      console.info(chalk.blue('[1/2] Load file from git'));
+    .then(choices => {
+      const { style } = choices
+      stepNum = style === 'afternoon-grocery' ? 1 : style === 'doc' ? 2 : 3
+      const branchName = style === 'afternoon-grocery' ? 'afternoon-grocery' : '1.x'
+      const gitBranch = `recoluan/vuepress-theme-reco-demo#demo/${branchName}`
+      spinner.start(chalk.blue(`[${currentStep}/${stepNum}] Load file from git`));
+      
       download(gitBranch, program.init, function (err) {
         if(!err){
-          console.info(chalk.blue('[2/2] Edit package.json'));
-          changePackage().then(() => {
-            spinner.stop()
-            console.log()
-            console.info(chalk.greenBright('Load successful, enjoy it!'));
-            console.log()
-            console.log(chalk.gray('# Inter your blog'))
-            console.log(`$ cd ${program.init}`)
-            console.log(chalk.gray('# Install package'))
-            console.log('$ npm install')
-          })
+          spinner.succeed(chalk.blue(`[${currentStep}/${stepNum}] Load file from git`));
+          currentStep++
+          handleDownload(choices)
         }else{
-          console.info(chalk.redBright('load fail!'));
+          spinner.fail(chalk.redBright(`[${currentStep}/${stepNum}] Load file from git`));
           console.info(err)
           spinner.stop()
         }
@@ -52,17 +51,79 @@ if (program.init) {
     })
 }  
 
-function changePackage () {
+function handleDownload(choices) {
+  if (choices.style === 'afternoon-grocery') {
+    handleEnd(spinner)
+    return
+  }
+
+  if (choices.style === 'doc') {
+    changePackage(choices).then(() => {
+      handleEnd(spinner)
+    })
+    return
+  }
+  
+  Promise.all([changePackage(choices), changeConfig(choices)]).then(() => {
+    handleEnd(spinner)
+  })
+}
+
+function handleEnd (spinner) {
+  spinner.stop()
+  console.log()
+  console.info(chalk.greenBright('Load successful, enjoy it!'));
+  console.log()
+  console.log(chalk.gray('# Inter your blog'))
+  console.log(`$ cd ${program.init}`)
+  console.log(chalk.gray('# Install package'))
+  console.log('$ npm install')
+}
+ 
+function changePackage (choices) {
+  spinner.start(chalk.blue(`[${currentStep}/${stepNum}] Edit package.json`));
+  
   return new Promise((resolve) => {
     fs.readFile(`${process.cwd()}/${program.init}/package.json`, (err, data) => {
       if (err) throw err;
       let _data = JSON.parse(data.toString())
       _data.name = program.init
+      _data.description= choices.description 
       _data.version = '1.0.0'
-      let str = JSON.stringify(_data, null, 4);
+      let str = JSON.stringify(_data, null, 2);
       fs.writeFile(`${process.cwd()}/${program.init}/package.json`, str, function (err) {
-        resolve()
-        if (err) throw err
+        if (!err) {
+          spinner.succeed(chalk.blue(`[${currentStep}/${stepNum}] Edit package.json`));
+          currentStep++
+          resolve()
+        } else {
+          spinner.fail(chalk.blue(`[${currentStep}/${stepNum}] Edit package.json`));
+          throw err
+        }
+      })
+    })
+  })
+}
+
+function changeConfig (choices) {
+  spinner.start(chalk.blue(`[${currentStep}/${stepNum}] Edit config.js`));
+  return new Promise((resolve) => {
+    fs.readFile(`${process.cwd()}/${program.init}/docs/.vuepress/config.js`, (err, data) => {
+      if (err) throw err;
+      const _data = eval(data.toString())
+      _data.themeConfig.type = 'blog'
+      _data.themeConfig.author = choices.author
+      _data.title = choices.title
+      _data.description = choices.description
+      let str = `module.exports = ${JSON.stringify(_data, null, 2)}`
+      fs.writeFile(`${process.cwd()}/${program.init}/docs/.vuepress/config.js`, str, function (err) {
+        if (!err) {
+          spinner.succeed(chalk.blue(`[${currentStep}/${stepNum}] Edit config.js`));
+          resolve()
+        } else {
+          spinner.fail(chalk.blue(`[${currentStep}/${stepNum}] Edit config.js`));
+          throw err
+        }
       })
     })
   })
